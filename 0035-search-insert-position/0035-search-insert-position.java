7f4144d831fc4e1203e6b35905c314c423d4f8f9
Bhavanam Vijay Pavan Kalyan Reddy <vijaypavankalyanreddy@gmail.com>
2026-09-17 18:59:35 +0530

    Time: 0 ms (100%), Space: 44.8 MB (29.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,26 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int size = nums.length;
+        int ans = nums.length;
         int left = 0;
-        int right = size-1;
-        int ans = -1;
+        int right = nums.length-1;
         while(left <= right)
         {
-            int middle = (left + right) / 2;
-            if(nums[middle] == target)
-            {
-                return middle;
-            }
-            else if(nums[middle] < target)
-            {
-                left = middle + 1;
-            }
-            else
+            int middle = left + (right - left) / 2;
+            if(nums[middle] >= target)
             {
                 ans = middle;
                 right = middle - 1;
             }
+            else
+            {
+                left = middle + 1;
+            }
         }
-        return ans == -1 ? size : ans;
+        return ans;
     }
 }
